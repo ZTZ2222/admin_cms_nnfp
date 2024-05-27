0346a61c90329d0a7278b76a7d3f34bf2732d109
ZTZ <tzamirov@gmail.com>
2024-05-28 00:51:49 +0600

    Implemented get_current_user function to retrieve user from database using OAuth2 token

--- fastapi-app/core/security.py
+++ fastapi-app/core/security.py
@@ -1,4 +1,14 @@
 import bcrypt
+from typing import Annotated
+from fastapi import Depends
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from core.models import db_helper, User
+from core.services.oauth2 import AuthService
+from utils.exceptions import InvalidCredentialsException
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/v1/login")
 
 
 def hash_password(password: str) -> str:
@@ -9,3 +19,17 @@ def hash_password(password: str) -> str:
 
 def verify_password(plain_password, hashed_password) -> bool:
     return bcrypt.checkpw(plain_password.encode('utf-8'), hashed_password.encode('utf-8'))
+
+
+async def get_current_user(
+    session: Annotated[
+        AsyncSession,
+        Depends(db_helper.session_getter),
+    ],
+    token: str = Depends(oauth2_scheme),
+) -> User:
+    token_data = AuthService.verify_access_token(token)
+    user_id = token_data.get("sub")
+    if user_id is None:
+        raise InvalidCredentialsException
+    return session.get(User, user_id)
