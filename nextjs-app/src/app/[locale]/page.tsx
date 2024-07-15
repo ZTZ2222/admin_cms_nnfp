9108a84a85872a55cc7a8b49e177c7d55fc7075f
ZTZ <tzamirov@gmail.com>
2024-07-15 12:46:36 +0600

    create index page

--- nextjs-app/src/app/[locale]/page.tsx
+++ nextjs-app/src/app/[locale]/page.tsx
@@ -1,3 +1,13 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Home() {
-  return <main>Home Page</main>;
+  return (
+    <main className="grid min-h-screen w-full place-content-center">
+      <div>User ID: Undefined</div>
+      <Button asChild variant="link" size="default">
+        <Link href="/sign-in">Go to login page</Link>
+      </Button>
+    </main>
+  );
 }
